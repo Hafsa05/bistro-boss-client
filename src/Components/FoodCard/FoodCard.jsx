7b5c6cca7c8b3f5eb80a4906437b25fe7ec60845
Hafsa05 <hafsa@g.bracu.ac.bd>
2023-05-29 02:32:14 +0600

    food card modified

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -2,7 +2,7 @@
 const FoodCard = ({ item }) => {
 	const { name, recipe, image, price, } = item;
 	return (
-		<div className="card w-96 bg-base-100 shadow-xl">
+		<div className="card w-96 bg-base-100 shadow-xl mb-10 mx-auto">
 			<figure><img src={image} alt="Shoes" /></figure>
 			<p className="bg-black text-white absolute right-0 mr-4 mt-4 px-4 py-1">${price}</p>
 			<div className="card-body flex flex-col items-center ">
